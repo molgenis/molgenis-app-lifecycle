39b0e5b96e9be8593c33fd70d8c3021e7165cd59
sido <sido@haakma.org>
2018-03-15 12:12:33 +0100

    chore(getters.spec.js): added tests to getters

--- test/unit/specs/store/getters.spec.js
+++ test/unit/specs/store/getters.spec.js
@@ -1,72 +1,45 @@
 import getters from '@/store/getters'
+import EntityV2Response  from '../mock-responses/EntityV2Response'
+import TreeMapperResponse from '../mock-responses/TreeMapperResponse'
+import ColumnsMapperResponse from '../mock-responses/ColumnsMapperResponse'
 
 describe('getters', () => {
-  const mockedState = {
+  const mockedTreeState = {
     tree: {
-      data: [
-        {
-          'id': 'p2',
-          'text': 'Parent2',
-          'value': 'Parent2',
-          'icon': 'fa fa-file-o',
-          'opened': true,
-          'selected': false,
-          'disabled': false,
-          'loading': false,
-          'children': []
-        },
-        {
-          'id': 'p1',
-          'text': 'Parent1',
-          'value': 'Parent1',
-          'icon': '',
-          'opened': true,
-          'selected': false,
-          'disabled': false,
-          'loading': false,
-          'children': [
-            {
-              'id': 'c1',
-              'text': 'Child1',
-              'value': 'Child1',
-              'icon': 'fa fa-file-o',
-              'opened': true,
-              'selected': false,
-              'disabled': false,
-              'loading': false,
-              'children': []
-            },
-            {
-              'id': 'c2',
-              'text': 'Child2',
-              'value': 'Child2',
-              'icon': '',
-              'opened': true,
-              'selected': false,
-              'disabled': false,
-              'loading': false,
-              'children': [
-                {
-                  'id': 'gc1',
-                  'text': 'Grandchild1',
-                  'value': 'Grandchild1',
-                  'icon': 'fa fa-file-o',
-                  'opened': true,
-                  'selected': false,
-                  'disabled': false,
-                  'loading': false,
-                  'children': []
-                }
-              ]
-            }
-          ]
-        }
-      ]
+      data: TreeMapperResponse.mockTreeData,
+      raw: EntityV2Response.mockRawTreeData
     }
   }
   it('getTreeData returns a clone of the tree data array in the state', () => {
-    const clone = getters.getTreeData(mockedState)
-    expect(clone).to.not.equal(mockedState.tree.data)
-    expect(clone).to.deep.equal(mockedState.tree.data)
+    const clone = getters.getTreeData(mockedTreeState)
+    expect(clone).to.not.equal(mockedTreeState.tree.data)
+    expect(clone).to.deep.equal(mockedTreeState.tree.data)
   })
+  it('getRawTreeData returns a clone of the raw tree data array in the state', () => {
+    const clone = getters.getRawTreeData(mockedTreeState)
+    expect(clone).to.not.equal(mockedTreeState.tree.raw)
+    expect(clone).to.deep.equal(mockedTreeState.tree.raw)
+  })
+  it('getCoreVariableColumns returns a clone of the et', () => {
+    const clone = getters.getCoreVariableColumns(ColumnsMapperResponse.mockColumns)
+    expect(clone).to.not.equal(ColumnsMapperResponse.mockColumns)
+    expect(clone).to.deep.equal(ColumnsMapperResponse.mockColumns)
+  })
+
+
+  getCoreVariableColumns: state => {
+    return _.cloneDeep(state.variables.columns)
+  },
+    getCoreVariableData: state => {
+    return _.cloneDeep(state.variables.data)
+  },
+    getCohorts: state => {
+    return _.cloneDeep(state.cohorts)
+  },
+    getHarmonizations: state => {
+    return _.cloneDeep(state.harmonizations)
+  },
+    getSourceVariables: state => {
+    return _.cloneDeep(state.variables.source)
+  }
 })
