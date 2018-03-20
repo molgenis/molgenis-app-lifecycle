775542d3dfd81bd63010f2758bafae11f2003273
mkslofstra <mariska-karina@hotmail.com>
2018-03-20 16:07:28 +0100

    Add back button

--- src/components/harmonization/HarmonizationComparison.vue
+++ src/components/harmonization/HarmonizationComparison.vue
@@ -5,14 +5,13 @@
 
     <div class="row mt-3">
       <div class="col-12">
-
         <div class="card">
           <div class="card-header">
             Harmonization of <strong>{{ selectedHarmonization.targetLabel }}</strong> and <strong>{{ selectedHarmonization.sourceLabel }}</strong>
           </div>
-
           <div class="card-body">
 
+            <router-link :to="'/'"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i> Back to filter</router-link>
             <div class="container-fluid">
               <div class="row">
                 <div class="col-xl-5 col-lg-5 col-12 mb-2">
