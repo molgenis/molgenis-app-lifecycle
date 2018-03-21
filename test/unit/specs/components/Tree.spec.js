34894068feec4b1e706a139f9e76bac1a7aa531e
mkslofstra <mariska-karina@hotmail.com>
2018-03-21 11:02:13 +0100

    Set up tree test

---
+++ test/unit/specs/components/Tree.spec.js
@@ -0,0 +1,8 @@
+// import Vue from 'vue'
+import Tree from '@/components/Tree'
+
+describe('Tree unit tests', () => {
+  it('should load "Tree" component as a name', () => {
+    expect(Tree.name).to.equal('Tree')
+  })
+})
