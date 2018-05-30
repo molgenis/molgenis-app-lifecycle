49776417c95cfc88ee292c5b003d7ad279ba779d
mkslofstra <mariska-karina@hotmail.com>
2018-05-30 11:07:26 +0200

    [FEATURE] Add tabs for tables

--- src/components/harmonization/Harmonizations.vue
+++ src/components/harmonization/Harmonizations.vue
@@ -1,34 +1,35 @@
 <template>
-  <div class="card" v-if="variableData.length > 0">
-    <div class="card-header">
-      Harmonizations of {{ variable }}
-    </div>
-    <div class="card-body table-responsive">
+  <div v-if="variableData.length > 0">
+    <h4>Harmonizations of {{ variable }}</h4>
+    <div class="table-responsive">
       <table class="table table-sm" cellspacing="0">
         <thead>
-          <tr>
-            <th></th>
-            <th v-for="variable in variableData" @click="selectedVariable = variable;showCoreVariable = !showCoreVariable">{{ variable.variable }}</th>
-          </tr>
-          <tr v-if="selectedVariable.variable && showCoreVariable">
-            <td></td>
-            <td>
-              <CoreVariableRow :variable="selectedVariable"></CoreVariableRow>
-            </td>
-          </tr>
+        <tr>
+          <th></th>
+          <th v-for="variable in variableData"
+              @click="selectedVariable = variable;showCoreVariable = !showCoreVariable">{{ variable.variable }}
+          </th>
+        </tr>
+        <tr v-if="selectedVariable.variable && showCoreVariable">
+          <td></td>
+          <td>
+            <CoreVariableRow :variable="selectedVariable"></CoreVariableRow>
+          </td>
+        </tr>
         </thead>
         <tbody v-for="cohort in cohorts">
-          <tr>
-            <td>{{ cohort.label }}</td>
-            <td v-for="variable in variableData">
-              <div v-if="variableExists(cohort.id, variable.harmonizations)" style="font-size:20px">
-                <router-link :to="'/' + variable.variable + '/' + getSelectedHarmonization(variable, cohort.id)"><i class="fa fa-check-circle text-success"></i></router-link>
-              </div>
-              <div v-else="!variableExists(cohort.id, variable.harmonizations)" style="font-size:20px; color:#970404">
-                <i class="fa fa-times-circle"></i>
-              </div>
-            </td>
-          </tr>
+        <tr>
+          <td>{{ cohort.label }}</td>
+          <td v-for="variable in variableData">
+            <div v-if="variableExists(cohort.id, variable.harmonizations)" style="font-size:20px">
+              <router-link :to="'/' + variable.variable + '/' + getSelectedHarmonization(variable, cohort.id)"><i
+                class="fa fa-check-circle text-success"></i></router-link>
+            </div>
+            <div v-else="!variableExists(cohort.id, variable.harmonizations)" style="font-size:20px; color:#970404">
+              <i class="fa fa-times-circle"></i>
+            </div>
+          </td>
+        </tr>
         </tbody>
       </table>
     </div>
