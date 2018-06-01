eef2f25eff9895faf8d5193a3599dd79936b74a8
Mark-de-Haan <markdehaan90@gmail.com>
2018-06-01 11:05:33 +0200

    Finish getter tests

--- test/unit/specs/store/getters.spec.js
+++ test/unit/specs/store/getters.spec.js
@@ -2,14 +2,44 @@ import getters from '@/store/getters'
 
 describe('getters', () => {
   const state = {
-    harmonizationData: [],
-    harmonizationMetadata: {}
+    harmonizationData: [
+      {id: 'entity1', sources: ['source1'], cohort: {id: 'cohort1'}, target: {variable: 'variable1'}},
+      {id: 'entity2', sources: ['source1'], cohort: {id: 'cohort1'}, target: {variable: 'variable2'}},
+      {id: 'entity3', sources: ['source1'], cohort: {id: 'cohort2'}, target: {variable: 'variable1'}},
+      {id: 'entity4', sources: ['source1'], cohort: {id: 'cohort2'}, target: {variable: 'variable3'}},
+      {id: 'entity5', sources: ['source1'], cohort: {id: 'cohort3'}, target: {variable: 'variable10'}}
+    ],
+    harmonizationMetadata: {
+      attributes: [
+        {
+          name: 'target',
+          refEntity: {
+            attributes: ['attr1', 'attr2']
+          }
+        }
+      ]
+    }
   }
 
   describe('getCohortVariableMapping', () => {
     it('should return a cohortVariableMapping based on the harmonization data in the state', () => {
       const actual = getters.getCohortVariableMapping(state)
-      const expected = {}
+      const expected = {
+        'cohort1': {
+          'variable1': {id: 'entity1', sources: ['source1']},
+          'variable2': {id: 'entity2', sources: ['source1']}
+        },
+
+        'cohort2': {
+          'variable1': {id: 'entity3', sources: ['source1']},
+          'variable3': {id: 'entity4', sources: ['source1']}
+        },
+
+        'cohort3': {
+          'variable10': {id: 'entity5', sources: ['source1']}
+        }
+      }
+
       expect(actual).to.deep.equal(expected)
     })
   })
@@ -17,7 +47,7 @@ describe('getters', () => {
   describe('getCoreVariableFields', () => {
     it('should return a list of core variable fields based on the harmonization metadata in the state', () => {
       const actual = getters.getCoreVariableFields(state)
-      const expected = []
+      const expected = ['attr1', 'attr2']
       expect(actual).to.deep.equal(expected)
     })
   })
@@ -25,7 +55,20 @@ describe('getters', () => {
   describe('getHarmonizationTable', () => {
     it('should return a harmonization table based on the harmonization data in the state', () => {
       const actual = getters.getHarmonizationTable(state)
-      const expected = {}
+      const expected = {
+        'cohort1': [
+          'variable1',
+          'variable2'
+        ],
+        'cohort2': [
+          'variable1',
+          'variable3'
+        ],
+        'cohort3': [
+          'variable10'
+        ]
+      }
+
       expect(actual).to.deep.equal(expected)
     })
   })
@@ -33,7 +76,7 @@ describe('getters', () => {
   describe('getSelectedHarmonization', () => {
     it('should return the selected harmonization based on the harmonization data in the state', () => {
       const actual = getters.getSelectedHarmonization(state)
-      const expected = undefined
+      const expected = {id: 'entity1', sources: ['source1'], cohort: {id: 'cohort1'}, target: {variable: 'variable1'}}
       expect(actual).to.deep.equal(expected)
     })
   })
